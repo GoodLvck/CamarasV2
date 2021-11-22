3073d8b7ad73cce61434674bc1591343e7a9c26b
GoodLvck <x17albaserrano17x@gmail.com>
2021-11-23 00:05:05 +0100

    temporary index and small changes

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<p>HOME</p>"
+    return render_template("index.html")
