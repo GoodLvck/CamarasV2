ec8857542f969167ae83da5a287c0aba61679817
GoodLvck <x17albaserrano17x@gmail.com>
2021-11-22 23:49:47 +0100

    language details and templates path changes

--- website/__init__.py
+++ website/__init__.py
@@ -5,7 +5,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'jnkcdskas'
 
     from .views import views
-    from. auth import auth
+    from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
