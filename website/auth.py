896b7a6a4e7a293c02cd97ef01c9b4f822e9f50d
GoodLvck <xlbaserrano17@gmail.com>
2022-01-31 00:39:39 +0100

    remove signup

--- website/auth.py
+++ website/auth.py
@@ -1,27 +1,22 @@
-from flask import Blueprint, render_template
+from crypt import methods
+from flask import Blueprint, render_template, request
 
 
 auth = Blueprint('auth', __name__)
 
 
 # -------------------- Login --------------------
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
-@auth.route('/es/login')
+@auth.route('/es/login', methods=['GET', 'POST'])
 def loginES():
+    data = request.form
+    print(data)
     return render_template("loginES.html")
-
-
-# -------------------- Signup --------------------
-@auth.route('/signup')
-def signup():
-    return render_template("signup.html")
-
-@auth.route('/es/signup')
-def signupES():
-    return render_template("signupES.html")
 
 
 # -------------------- Logout --------------------
