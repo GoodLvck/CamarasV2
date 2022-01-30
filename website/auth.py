1e32e7548a243fb10e1fbb3205701c33e3f03dd0
GoodLvck <xlbaserrano17@gmail.com>
2022-01-31 00:09:37 +0100

    signup provisional

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template
 
+
 auth = Blueprint('auth', __name__)
 
+
+# -------------------- Login --------------------
 @auth.route('/login')
 def login():
     return render_template("login.html")
@@ -10,10 +13,18 @@ def login():
 def loginES():
     return render_template("loginES.html")
 
+
+# -------------------- Signup --------------------
+@auth.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+@auth.route('/es/signup')
+def signupES():
+    return render_template("signupES.html")
+
+
+# -------------------- Logout --------------------
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
-
-@auth.route('/sign-up')
-def signup():
-    return "<p>sign Up</p>"
