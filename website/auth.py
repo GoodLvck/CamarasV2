ee30f42a909a9d37d943bd3996ee14e8b6f315f4
GoodLvck <xlbaserrano17@gmail.com>
2022-02-01 12:42:36 +0100

    Signup and messages

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,5 @@
 from crypt import methods
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 
 auth = Blueprint('auth', __name__)
@@ -8,15 +8,31 @@ auth = Blueprint('auth', __name__)
 # -------------------- Login --------------------
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
     return render_template("login.html")
 
 @auth.route('/es/login', methods=['GET', 'POST'])
 def loginES():
-    data = request.form
-    print(data)
     return render_template("loginES.html")
+
+
+# -------------------- Signup --------------------
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(username) < 3:
+            flash('Username must be greater than 3 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 8:
+            flash('Password must be at least 8 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+
+    return render_template("signup.html")
 
 
 # -------------------- Logout --------------------
--- website/static/index.js
+++ website/static/index.js
@@ -13,3 +13,19 @@ function password_show_hide() {
     show_eye.style.display = "none";
   }
 }
+
+function password_show_hide2() {
+  var x = document.getElementById("confirmPassword");
+  var show_eye = document.getElementById("show_eye2");
+  var hide_eye = document.getElementById("hide_eye2");
+  show_eye.classList.remove("d-none");
+  if (x.type === "password") {
+    x.type = "text";
+    hide_eye.style.display = "none";
+    show_eye.style.display = "block";
+  } else {
+    x.type = "password";
+    hide_eye.style.display = "block";
+    show_eye.style.display = "none";
+  }
+}
